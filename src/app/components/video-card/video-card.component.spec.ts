2ab34114e931f9c495aec65982b31ee28ec992b6
eeshan <eeshanj@gmail.com>
2020-08-04 22:17:46 +0530

    list view video page and video card component added

---
+++ src/app/components/video-card/video-card.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { VideoCardComponent } from './video-card.component';
+
+describe('VideoCardComponent', () => {
+  let component: VideoCardComponent;
+  let fixture: ComponentFixture<VideoCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VideoCardComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(VideoCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/video-card/video-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-card',
+  templateUrl: './video-card.component.html',
+  styleUrls: ['./video-card.component.scss'],
+})
+export class VideoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

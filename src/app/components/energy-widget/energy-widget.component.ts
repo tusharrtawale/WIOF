4ab79fa9eddb822ed580701d1c1c73025d8c296c
eeshan <eeshanj@gmail.com>
2020-08-16 12:57:41 +0530

    energy widget and minor updates

--- src/app/components/energy-widget/energy-widget.component.ts
+++ src/app/components/energy-widget/energy-widget.component.ts
@@ -7,14 +7,44 @@ import { NgForm} from '@angular/forms';
   styleUrls: ['./energy-widget.component.scss'],
 })
 export class EnergyWidgetComponent implements OnInit {
-
+  EnergyConsumption:number;
+  KgsofCO2:number ;
+  Selectedunit = "";
+  litresofPetrol:number;
+  litresofDiesel:number;
+  KmsbySUV;
+  kmsbycar;
+  buttonClicked=false;
+  
   constructor() { }
 
   ngOnInit() {}
 
+ CalculateCO2(){
+  this.buttonClicked=true;
+if (this.Selectedunit=="1")
+{
+  
+  this.KgsofCO2=this.EnergyConsumption*612;
+  console.log(this.Selectedunit);
+  console.log(typeof(this.Selectedunit));
+ // console.log( this.KgsofCO2=this.EnergyConsumption*0);
 
-  onSubmit(form: NgForm){
+ this.litresofPetrol=this.KgsofCO2/2.296;
+  this.litresofDiesel=this.KgsofCO2/2.653;
+  this.kmsbycar=this.litresofPetrol*15;
+  this.KmsbySUV=this.litresofDiesel*12;
+}
 
-    console.log(form);
-  }
+else{
+this.KgsofCO2= this.EnergyConsumption *0.85
+console.log(this.KgsofCO2);  
+
+  this.litresofPetrol=this.KgsofCO2/2.296;
+  this.litresofDiesel=this.KgsofCO2/2.653;
+  this.kmsbycar=this.litresofPetrol*15;
+  this.KmsbySUV=this.litresofDiesel*12;
+}
+ }
+
 }

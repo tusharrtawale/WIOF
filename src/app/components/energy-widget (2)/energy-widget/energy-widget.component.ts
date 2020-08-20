1dc81db23263a6e2f9114968f53d17f05948f103
eeshan <eeshanj@gmail.com>
2020-08-20 22:03:34 +0530

    social share buttons, energy widget, back button, other styling

--- src/app/components/energy-widget (2)/energy-widget/energy-widget.component.ts
+++ src/app/components/energy-widget (2)/energy-widget/energy-widget.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm} from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-energy-widget',
@@ -21,7 +22,6 @@ export class EnergyWidgetComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {}
-
 
  CalculateCO2(){
  
@@ -93,34 +93,6 @@ this.Consumptionnull=true;
 //   return this.unitnull;
 // }
 }
+ }
+ 
 
-  //old
-
-//  CalculateCO2(){
-//   this.buttonClicked=true;
-// if (this.Selectedunit=="1")
-// {
-  
-//   this.KgsofCO2=this.EnergyConsumption*612;
-//   console.log(this.Selectedunit);
-//   console.log(typeof(this.Selectedunit));
-//  // console.log( this.KgsofCO2=this.EnergyConsumption*0);
-
-//  this.litresofPetrol=this.KgsofCO2/2.296;
-//   this.litresofDiesel=this.KgsofCO2/2.653;
-//   this.kmsbycar=this.litresofPetrol*15;
-//   this.KmsbySUV=this.litresofDiesel*12;
-// }
-
-// else{
-// this.KgsofCO2= this.EnergyConsumption *0.85
-// console.log(this.KgsofCO2);  
-
-//   this.litresofPetrol=this.KgsofCO2/2.296;
-//   this.litresofDiesel=this.KgsofCO2/2.653;
-//   this.kmsbycar=this.litresofPetrol*15;
-//   this.KmsbySUV=this.litresofDiesel*12;
-// }
-//  }
-
-}
--- src/app/components/social-share/social-share.component.ts
+++ src/app/components/social-share/social-share.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-social-share',
@@ -6,9 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social-share.component.scss'],
 })
 export class SocialShareComponent implements OnInit {
+  // @Input() title:string="Get to know more at WorldIsOneFamily.com";
+  title:string="Get to know more at WorldIsOneFamily.com";
+  url:string=window.location.href;
+  facebookShareLink:string=`https://www.facebook.com/sharer.php?u=${this.url}`;
+  // https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+  linkedinShareLink:string=`https://www.linkedin.com/shareArticle?url=${this.url}&title=${this.title}`;
+  twitterShareLink:string=`https://twitter.com/share?url=${this.url}&text=${this.title}`;
+  whatsappShareLink:string=`https://api.whatsapp.com/send?text=${this.title} ${this.url}`;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
 
 }

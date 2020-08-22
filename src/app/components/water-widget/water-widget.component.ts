5878e4c9ae2cc503afa70565c6c1a095fcc37960
Naved <nmaulavi5@gmail.com>
2020-08-22 18:43:17 +0530

    fixed water widget icon css issue

--- src/app/components/water-widget/water-widget.component.ts
+++ src/app/components/water-widget/water-widget.component.ts
@@ -6,7 +6,46 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./water-widget.component.scss"],
 })
 export class WaterWidgetComponent implements OnInit {
+  waterWidgetItems: { name: string; icon: string; url: string }[];
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.waterWidgetItems = [
+      {
+        name: "Ground Water",
+        icon: "../../../assets/water-widget-icons/icons ground water.png",
+        url: "https://indiawris.gov.in/wris/#/groundWater",
+      },
+      {
+        name: "Rainfall",
+        icon: "../../../assets/water-widget-icons/icons rainfall.png",
+        url: "https://indiawris.gov.in/wris/#/rainfall",
+      },
+      {
+        name: "Evapo-Transpiration",
+        icon: "../../../assets/water-widget-icons/icons8-sparkling_water 1.png",
+        url: "https://indiawris.gov.in/wris/#/evapotranspiration",
+      },
+      {
+        name: "Reservoirs",
+        icon: "../../../assets/water-widget-icons/icons reservoir.png",
+        url: "https://indiawris.gov.in/wris/#/Reservoirs",
+      },
+      {
+        name: "River",
+        icon: "../../../assets/water-widget-icons/icons river.png",
+        url: "https://indiawris.gov.in/wris/#/RiverMonitoring",
+      },
+      {
+        name: "Soil Moisture",
+        icon: "../../../assets/water-widget-icons/icons soil moisture.png",
+        url: "https://indiawris.gov.in/wris/#/soilMoisture",
+      },
+      {
+        name: "Surface Water Quality",
+        icon: "../../../assets/water-widget-icons/icon surface water quality.png",
+        url: "https://indiawris.gov.in/wris/#/SWQuality",
+      },
+    ];
+  }
 }

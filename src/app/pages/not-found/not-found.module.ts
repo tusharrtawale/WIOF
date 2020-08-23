72bbafdb3c4912af2c073718bcbe06c4dffcc02d
Naved <nmaulavi5@gmail.com>
2020-08-23 11:58:06 +0530

    prod build compilation issue solved

--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-
 import { IonicModule } from "@ionic/angular";
-
 import { NotFoundPageRoutingModule } from "./not-found-routing.module";
-
 import { NotFoundPage } from "./not-found.page";
+import { AppCommonModule } from "src/app/app-common.module";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, NotFoundPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    NotFoundPageRoutingModule,
+    AppCommonModule,
+  ],
   declarations: [NotFoundPage],
 })
 export class NotFoundPageModule {}

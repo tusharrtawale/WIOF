e4ceb9e76232e8ca5c9d6dc3e81035a329b3eca4
eeshan <eeshanj@gmail.com>
2020-08-28 21:12:46 +0530

    concent popup

--- src/app/components/concent-popup/concent-popup.component.ts
+++ src/app/components/concent-popup/concent-popup.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-concent-popup',
   templateUrl: './concent-popup.component.html',
   styleUrls: ['./concent-popup.component.scss'],
+  exportAs:'concentPopup'
 })
 export class ConcentPopupComponent implements OnInit {
-
+  @Output() accepted=new EventEmitter;
+  read:boolean=false;
   constructor() { }
 
   ngOnInit() {}
-
+  onAccept(){
+    this.read=true;
+    this.accepted.emit("concentAccepted");
+  }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { ElementRef, ViewChild } from "@angular/core";
+
+
 
 @Component({
   selector: "app-home",
@@ -6,5 +9,16 @@ import { Component } from "@angular/core";
   styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  // @ViewChild("cp", {read: ElementRef}) cp: ElementRef;
+  viewConcentPopup=true;
+
+  // ngAfterViewInit(): void {
+    // outputs `I am span`
+    // console.log(this.cp.nativeElement.read);
+// }
+
   constructor() {}
+  onAccept(){    
+    this.viewConcentPopup=false;
+  }
 }

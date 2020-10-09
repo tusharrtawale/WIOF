77894a435474370da6c7979fb20834cae4333c2b
eeshan <eeshanj@gmail.com>
2020-10-09 18:39:24 +0530

    update2 after pull

--- src/app/app-common.module.ts
+++ src/app/app-common.module.ts
@@ -74,10 +74,7 @@ import { BreakingNewsComponent } from "./components/breaking-news/breaking-news.
     SocialShareComponent,
     VideoCardComponent,
     BreakingNewsComponent,
-<<<<<<< HEAD
     EqWidgetTestComponent
-=======
->>>>>>> 66a19e91c0bdc9d63f78477b8a6755b1a64df0b5
   ],
   declarations: [
     HeaderComponent,
@@ -107,11 +104,8 @@ import { BreakingNewsComponent } from "./components/breaking-news/breaking-news.
     SocialShareComponent,
     VideoCardComponent,
     BreakingNewsComponent,
-<<<<<<< HEAD
     EqWidgetTestComponent
 
-=======
->>>>>>> 66a19e91c0bdc9d63f78477b8a6755b1a64df0b5
   ],
   providers: [],
 })

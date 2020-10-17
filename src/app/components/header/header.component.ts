9f63936b053b1d78fe75c614d83812c59a29f04f
Naved <nmaulavi5@gmail.com>
2020-10-17 21:24:54 +0530

    fixed ios issue

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,25 +8,7 @@ import { Platform } from "@ionic/angular";
 })
 export class HeaderComponent implements OnInit {
   wiofLogo: string = "../../assets/logo.jpg";
-  isIos: boolean;
   constructor(public platform: Platform) {}
 
-  ngOnInit() {
-    this.isIos = this.testiOS();
-  }
-
-  testiOS() {
-    return (
-      [
-        "iPad Simulator",
-        "iPhone Simulator",
-        "iPod Simulator",
-        "iPad",
-        "iPhone",
-        "iPod",
-      ].includes(navigator.platform) ||
-      // iPad on iOS 13 detection
-      (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-    );
-  }
+  ngOnInit() {}
 }

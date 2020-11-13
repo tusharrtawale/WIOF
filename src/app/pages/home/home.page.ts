b1438691f7128fb605293914bb958c0b2675edab
Naved <nmaulavi5@gmail.com>
2020-11-13 18:40:15 +0530

    Added inauguration code

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,22 +1,40 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ElementRef, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
-  styleUrls: ["home.page.scss"]
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
-  // @ViewChild("cp", {read: ElementRef}) cp: ElementRef;
+export class HomePage implements OnInit {
   viewConcentPopup = true;
-
-  // ngAfterViewInit(): void {
-  // outputs `I am span`
-  // console.log(this.cp.nativeElement.read);
-  // }
+  ribbonCut = false;
+  counter = 5;
+  buttonClicked = false;
 
   constructor() {}
+
+  ngOnInit() {
+    this.ribbonCut = !!localStorage.getItem("ribbonCut");
+  }
+
   onAccept() {
     this.viewConcentPopup = false;
+  }
+
+  onOpening() {
+    this.buttonClicked = true;
+    setInterval(() => {
+      this.counter--;
+    }, 1000);
+
+    setTimeout(() => {
+      this.ribbonCut = true;
+      localStorage.setItem("ribbonCut", "true");
+      setTimeout(() => {
+        const curtainEl = document.getElementById("curtain-id");
+        curtainEl.style.display = "none";
+      }, 3000);
+    }, 5000);
   }
 }

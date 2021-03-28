669c9b199a16789e58a85ed045818039f0b4eb0a
Naved <nmaulavi5@gmail.com>
2021-03-28 13:29:16 +0530

    Added coffee conversation component and prettier config

---
+++ src/app/components/coffee-conversation/coffee-conversation.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee-conversation',
+  templateUrl: './coffee-conversation.component.html',
+  styleUrls: ['./coffee-conversation.component.scss'],
+})
+export class CoffeeConversationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

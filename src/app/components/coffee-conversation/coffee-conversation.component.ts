533908413fea55c9d24c61796e520ce25e90aad1
Naved Maulavi <nmaulavi5@gmail.com>
2021-08-09 22:02:05 +0530

    fixed UI issues

--- src/app/components/coffee-conversation/coffee-conversation.component.ts
+++ src/app/components/coffee-conversation/coffee-conversation.component.ts
@@ -12,11 +12,18 @@ export class CoffeeConversationComponent implements OnInit, OnDestroy {
   @Input() coffeeConvList: Array<CoffeeConversation>;
   @Input() element: string;
   @Input() fromHomePage: boolean;
+  showAboutInterviewee = false;
 
   slideOpts = COFFEE_CONV_SLIDER_OPTIONS;
   coffeeConvClass: string;
 
   constructor(private appUtilService: AppUtilService) {}
+
+  showIntervieweeDetails(event, value) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.showAboutInterviewee = value;
+  }
 
   ngOnInit() {
     this.coffeeConvClass = `wiof-${this.element}`;
